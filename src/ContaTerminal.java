a64a495ee0075c861ea58a8f3d885f0365e9e49a
Biel <gkortschinski@gmail.com>
2025-05-15 02:56:32 -0300

    Sistema de escanear dados

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,8 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
 
-        }
+        System.out.println("Por favor, digite o seu nome!");
+        String nomeCliente = scan.nextLine();
+
+        System.out.println("Por favor, digite o numero da sua agência!");
+        String numeroAgencia = scan.nextLine();
+
+        System.out.println("Por favor, digite o numero da sua conta!");
+        int numeroConta = scan.nextInt();
+
+        System.out.println("Por favor, digite o valor dentro do seu saldo!");
+        double saldo = scan.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco!");
+        System.out.println("O numero de sua agência é " + numeroAgencia + ", o numero de sua conta é " + numeroConta + " e seu saldo " + saldo + " já está disponivel para saque.");
     }
 }
