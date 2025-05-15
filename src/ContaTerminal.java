98cff7f24b532269dbac71de460e51399d9b4a9f
Biel <gkortschinski@gmail.com>
2025-05-15 03:10:26 -0300

    Sistema de captura de formatada de entrada invalida

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,23 +1,31 @@
 import java.util.Scanner;
 import java.util.Locale;
+import java.util.InputMismatchException;
+
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+        try (Scanner scan = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("Por favor, digite o seu nome!");
+            String nomeCliente = scan.nextLine();
 
-        System.out.println("Por favor, digite o seu nome!");
-        String nomeCliente = scan.nextLine();
+            System.out.println("Por favor, digite o numero da sua agência!");
+            String numeroAgencia = scan.nextLine();
 
-        System.out.println("Por favor, digite o numero da sua agência!");
-        String numeroAgencia = scan.nextLine();
+            System.out.println("Por favor, digite o numero da sua conta!");
+            int numeroConta = scan.nextInt();
 
-        System.out.println("Por favor, digite o numero da sua conta!");
-        int numeroConta = scan.nextInt();
+            System.out.println("Por favor, digite o valor do seu saldo!");
+            double saldo = scan.nextDouble();
 
-        System.out.println("Por favor, digite o valor dentro do seu saldo!");
-        double saldo = scan.nextDouble();
+            System.out.println("Olá " + nomeCliente
+                    + ", obrigado por criar uma conta em nosso banco!");
+            System.out.println("Agência: " + numeroAgencia
+                    + " | Conta: " + numeroConta
+                    + " | Saldo disponível: " + saldo);
+        } catch (InputMismatchException e){
+            System.out.println("Erro : Formato de entrada invalido");
+        }
 
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco!");
-        System.out.println("O numero de sua agência é " + numeroAgencia + ", o numero de sua conta é " + numeroConta + " e seu saldo " + saldo + " já está disponivel para saque.");
     }
 }
