ad4e0d506914784c53f5cee245077a16171b25ae
Biel <gkortschinski@gmail.com>
2025-05-22 16:31:19 -0300

    Ordenação por ID na to-do list adicionado

--- src/SistemaBanco/model/GestaoDeTarefas.java
+++ src/SistemaBanco/model/GestaoDeTarefas.java
@@ -1,4 +1,6 @@
 package SistemaBanco.model;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -7,17 +9,29 @@ public class GestaoDeTarefas {
         // Criando uma lista de tarefas
         GestaoDeTarefas primeiraLista = new GestaoDeTarefas();
         // Adicionado tarefas
-        primeiraLista.adicionarTarefa("Estudar Java", "Alta");
-        primeiraLista.adicionarTarefa("Ir pra academia", "Alta");
-        primeiraLista.adicionarTarefa("Chutar o Juan", "Media");
-        primeiraLista.adicionarTarefa("Massagear o gato", "Baixa");
-        primeiraLista.adicionarTarefa("Ir a praia", "Baixa");
-        // Concluindo tarefas
-        primeiraLista.TarefaConcluida("Massagear o gato");
-        primeiraLista.TarefaConcluida("Estudar Java");
+        primeiraLista.adicionarTarefa("Estudar Java", "Alta", 10);
+        primeiraLista.adicionarTarefa("Ir pra academia", "Alta", 20);
+        primeiraLista.adicionarTarefa("Chutar o Juan", "Media", 30);
+        primeiraLista.adicionarTarefa("Bangar o time", "Media", 11);
+        primeiraLista.adicionarTarefa("Massagear o gato", "Baixa", 4);
+        primeiraLista.adicionarTarefa("Ir a praia", "Baixa", 6);
         // Listando tarefas pela prioridade
+        System.out.print("\n");
         primeiraLista.listarTarefas("Alta");
+        System.out.print("\n");
         primeiraLista.listarTarefas("Media");
+        System.out.print("\n");
+        primeiraLista.listarTarefas("Baixa");
+        // Concluindo tarefas
+        System.out.print("\n");
+        primeiraLista.TarefaConcluida("Massagear o gato");
+        primeiraLista.TarefaConcluida("Ir pra academia");
+        // Listando tarefas pela prioridade
+        System.out.print("\n");
+        primeiraLista.listarTarefas("Alta");
+        System.out.print("\n");
+        primeiraLista.listarTarefas("Media");
+        System.out.print("\n");
         primeiraLista.listarTarefas("Baixa");
     }
 
@@ -27,9 +41,9 @@ public class GestaoDeTarefas {
         this.listinha = new ArrayList<>();
     }
 
-    public void adicionarTarefa (String descricao, String prioridade) {
-        listinha.add(new Tarefa(descricao, prioridade));
-        System.out.println("Foi adicionada a tarefa com a descrição : " + descricao);
+    public void adicionarTarefa (String descricao, String prioridade, int id) {
+        listinha.add(new Tarefa(descricao, prioridade, id));
+        System.out.println("Foi adicionada a tarefa com a descrição : " + descricao + "(ID=" + id + ")");
     }
 
     public void TarefaConcluida(String descricao) {
@@ -45,28 +59,44 @@ public class GestaoDeTarefas {
     }
 
     public void listarTarefas (String prioridade) {
-        System.out.println("Listando tarefas de prioridade " + prioridade);
+        System.out.println("Listando tarefas de prioridade " + prioridade + " ordenado pelo ID :");
+        List<Tarefa> listaDePrioridade = new ArrayList<>();
         for(Tarefa t : listinha) {
             if(t.getPrioridade().equalsIgnoreCase(prioridade)) {
-                System.out.println(t.getDescricao());
+                listaDePrioridade.add(t);
             }
+        }
+        Collections.sort(listaDePrioridade);
+        for(Tarefa b : listaDePrioridade) {
+            System.out.println(b.getDescricao() + " (ID=" + b.getId() + ")");
         }
     }
 }
 
-class Tarefa {
+class Tarefa implements Comparable<Tarefa> {
     private String descricao;
     private String prioridade;
+    private int id;
     private Boolean concluida;
 
-    public Tarefa(String descricao, String prioridade) {
+    public Tarefa(String descricao, String prioridade, int id) {
         this.descricao = descricao;
         this.prioridade = prioridade;
         this.concluida = false;
+        this.id = id;
+    }
+
+    @Override
+    public int compareTo(Tarefa t) {
+        return Integer.compare(id, t.getId());
     }
 
     public void setEstado() {
         this.concluida = true;
+    }
+
+    public int getId () {
+        return id;
     }
 
     public Boolean getEstado () {
